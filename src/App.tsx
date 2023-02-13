b6340c433ac4655cebcbc84d08b10efc31596c80
Kevin Araujo <kevinaraujo@Kevin-Araujos-MacBook-Pro-ArcTouch-447A.local>
2023-02-13 15:55:19 -0300

    feat: Created list component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Form from './components/Form';
+import Lista from './components/List';
 
 function App() {
   return (
     <div className="App">
      <Form/>
+     <Lista/>
     </div>
   );
 }
---
+++ src/components/List/index.tsx
@@ -0,0 +1,30 @@
+import { render } from '@testing-library/react'
+import React from 'react'
+
+function Lista() {
+    return (
+        <aside>
+            <h2>Study of the day</h2>
+            <ul>
+                <li>
+                    <h3>
+                        React
+                    </h3>
+                    <span>
+                        02:00:00
+                    </span>
+                </li>
+                <li>
+                    <h3>
+                        Javascript
+                    </h3>
+                    <span>
+                        01:00:00
+                    </span>
+                </li>
+            </ul>
+        </aside>
+    )
+}
+
+export default Lista
