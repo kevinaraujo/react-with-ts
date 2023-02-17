e7b71cca83f180e47a891a21eb8340f171ae621a
Kevin Araujo <kevinaraujo@Kevin-Araujos-MacBook-Pro-ArcTouch-447A.local>
2023-02-17 17:24:29 -0300

    fix: Created task state.

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,21 +1,25 @@
 import { render } from '@testing-library/react'
-import React from 'react'
+import React, { useState } from 'react'
 import Item from './item'
 
 import style from './style.module.scss'
 
 function List() {
-    const tasks = [{
+    let [ tasks, setTasks ] = useState([{
         task: 'React',
         time: ' 02:00:00'
     }, {
         task: 'Javascript',
         time: ' 02:00:00'
-    }]
+    }])
+    
 
     return (
         <aside className={style.listaTarefas}>
-            <h2>Study of the day</h2>
+            <h2 onClick={() => {
+                tasks = [...tasks, { task: 'ssss', time: ' 02:00:00' }]
+                setTasks(tasks)
+            }}>Study of the day</h2>
             <ul>
                 { tasks.map((item, index) => (
                     <Item 
