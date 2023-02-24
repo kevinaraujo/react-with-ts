3abe11a2a749e5bb4b66914d10a7f4a49f51bef6
Kevin Araujo <kevinaraujo@Kevin-Araujos-MacBook-Pro-ArcTouch-447A.local>
2023-02-24 11:41:52 -0300

    feat: Used selected time on Chronometer component to start counting.

--- src/components/Chronometer/index.tsx
+++ src/components/Chronometer/index.tsx
@@ -2,13 +2,27 @@ import Button from "../Button"
 import Watch from "./Watch"
 
 import style from './style.module.scss'
-import { timeToSeconds } from "../../common/utils/date"
+import { timeToSeconds } from "../../common/utils/time"
+import { ITask } from "../../types/ITask"
+import { useEffect, useState } from "react"
 
-const Chronometer = () => {
-    console.log('converstion', timeToSeconds('01:01:01'))
+interface IProps {
+    selected: ITask | undefined   
+}
+
+const Chronometer = ({ selected }: IProps) => {
+    const [time, setTime] = useState<number>()
+
+   useEffect(() => {
+    if (selected?.time) {
+        setTime(timeToSeconds(selected?.time))
+    }
+   }, [selected])
+
     return (
         <div className={style.cronometro}>
-            <p className={style.titulo}>Escolha um card e inicie o cronômetro</p>
+            <p className={style.titulo}>Choose a card and start the chronometer</p>
+            Time: {time}
             <div className={style.relogioWrapper}>
                 <Watch />
             </div>
